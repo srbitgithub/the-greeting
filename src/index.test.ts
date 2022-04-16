9d60e443035c634a2fcea1dc7b501836f6417730
srbitgithub <sr_bit@hotmail.com>
2022-04-16 11:26:05 +0200

    Return Hello <name> when receive a name and Hello my friend when no receive any argument

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,25 @@
-import { test } from "./index"
+import { Greeting } from "./index"
 
 describe("The Greeting", () => {
-  it("returns 'That code works fine'", () => {
-    const expected = "That code works fine";
-
-    const result = test();
-
-    expect(result).toEqual(expected);
+  it("returns 'Hello Bob' when reciebe Bob as argument", () => {
+    const expected = 'Hello Bob';
+    const greeting = new Greeting('Bob');
+    console.clear()
+    expect(greeting.result).toEqual(expected);
   })
+
+  it("returns another name when reciebe any name as argument", () => {
+    const expected = 'Hello Michael';
+    const greeting = new Greeting('Michael');
+    console.clear()
+    expect(greeting.result).toEqual(expected);
+  })
+
+  it("returns 'Hello my friend' when not reciebe any argument", () => {
+    const expected = 'Hello my friend';
+    const greeting = new Greeting();
+    console.clear()
+    expect(greeting.result).toEqual(expected);
+  })
+
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-export function test(): any {
-    return "That code works fine"
-  };
+export class Greeting {
+  receivedName:string = ''
+  result:string = ''
+
+  constructor(_receivedName?:string){
+    if (_receivedName) this.receivedName = _receivedName
+    else this.receivedName = 'my friend'
+    this.main()
+  }
+  main(){
+    this.result = `Hello ${this.receivedName}`
+  }
+};
   
