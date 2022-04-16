855e2e212daa82cbf6dba5bd41ab7ed1d715064f
srbitgithub <sr_bit@hotmail.com>
2022-04-16 19:53:31 +0200

    Function array upperCase and lowerCase added

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,37 +2,58 @@ import { Greeting } from "./index"
 
 describe("The Greeting", () => {
   it("returns 'Hello Bob' when reciebe Bob as argument", () => {
+    console.clear()
     const expected = 'Hello Bob';
     const greeting = new Greeting('Bob');
-    console.clear()
     expect(greeting.result).toEqual(expected);
   })
 
   it("returns another name when reciebe any name as argument", () => {
+    console.clear()
     const expected = 'Hello Michael';
     const greeting = new Greeting('Michael');
-    console.clear()
     expect(greeting.result).toEqual(expected);
   })
 
   it("returns 'Hello my friend' when not reciebe any argument", () => {
+    console.clear()
     const expected = 'Hello my friend';
     const greeting = new Greeting();
-    console.clear()
     expect(greeting.result).toEqual(expected);
   })
 
   it("returns the greeting in upperCase when received argument is upperCase", () => {
+    console.clear()
     const expected = 'HELLO RAMBO';
     const greeting = new Greeting('RAMBO');
-    console.clear()
     expect(greeting.result).toEqual(expected);
   })
 
   it("returns the greeting with two names when received argument is an array with two names", () => {
-    const expected = 'Hello Rocky and Rambo';
-    const greeting = new Greeting(['Rocky', 'Rambo']);
     console.clear()
+    const expected = 'Hello, Rocky and Rambo';
+    const greeting = new Greeting(['Rocky', 'Rambo']);
+    expect(greeting.result).toEqual(expected);
+  })
+
+  it("returns the greeting with more then two names when received argument is an array with more than two names", () => {
+    console.clear()
+    const expected = 'Hello, Conan, Rocky and Rambo';
+    const greeting = new Greeting(['Conan', 'Rocky', 'Rambo']);
+    expect(greeting.result).toEqual(expected);
+  })
+
+  it("Separate names by lowerCase and upperCas and greeting in lowerCase or upperCase", () => {
+    console.clear()
+    const expected = 'Hello, Conan and Rambo. AND HELLO ROCKY';
+    const greeting = new Greeting(['Conan', 'ROCKY', 'Rambo']);
+    expect(greeting.result).toEqual(expected);
+  })
+
+  it("Separate names by lowerCase and upperCas and greeting in lowerCase or upperCase", () => {
+    console.clear()
+    const expected = 'Hello, Conan and Rambo. AND HELLO, ROCKY AND DANDIE';
+    const greeting = new Greeting(['Conan', 'ROCKY', 'Rambo', 'DANDIE']);
     expect(greeting.result).toEqual(expected);
   })
 
--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,10 @@ export class Greeting {
 
   constructor(_receivedName?:any){
     if (!_receivedName) this.receivedArrayNames = ['my friend']
+
     if (_receivedName && (typeof _receivedName === 'string'))
       this.receivedArrayNames = [_receivedName]
+
     if (_receivedName && (Array.isArray(_receivedName)))
       this.receivedArrayNames = _receivedName
      
@@ -22,11 +24,53 @@ export class Greeting {
   }
   
   main(){
+    const lowerCaseStrings:string[] = []
+    const upperCaseStrings:string[] = []
+
     if (this.receivedArrayNames.length === 1)
       this.result = `Hello ${this.receivedArrayNames[0]}`
-    if (this.receivedArrayNames.length === 2) 
-      this.result = `Hello ${this.receivedArrayNames[0]} and ${this.receivedArrayNames[1]}`
+    if (this.receivedArrayNames.length > 1) this.separateByUpperCaseAndLowerCase()
     if (this.receivedArrayNames.length > 0 && this.receivedArrayNames[0] === this.receivedArrayNames[0].toUpperCase())
       this.result = this.result.toUpperCase()
   }
+
+  textToReturnLowerCase(arrayNames:string[]):string{
+    let textToReturn:string = "Hello"
+    for (let i:number = 0; i < arrayNames.length - 1; i++){
+      textToReturn = textToReturn + ", " + arrayNames[i]
+    }
+    textToReturn = textToReturn + " and " + arrayNames[arrayNames.length - 1]
+    return textToReturn
+  }
+
+  textToReturnUpperCase(arrayNames:string[]):string{
+    let textToReturn:string = ". AND HELLO"
+    for (let i:number = 0; i < arrayNames.length - 1; i++){
+      textToReturn = textToReturn + ", " + arrayNames[i]
+    }
+    if (arrayNames.length == 1) textToReturn = textToReturn + " " + arrayNames[arrayNames.length - 1]
+    if (arrayNames.length > 1) textToReturn = textToReturn + " AND " + arrayNames[arrayNames.length - 1]
+
+    return textToReturn
+  }
+
+  separateByUpperCaseAndLowerCase(){
+    let IS_UPPERCASE:boolean = true
+    let IS_LOWERCASE:boolean = false
+
+    const lowerCaseStrings:string[] = []
+    const upperCaseStrings:string[] = []
+
+    let textToSetInResult:string = ''
+
+    this.receivedArrayNames.forEach(item => {
+      if (item === item.toUpperCase()) upperCaseStrings.push(item)
+      else lowerCaseStrings.push(item)
+    });
+    if (lowerCaseStrings.length > 0) textToSetInResult = textToSetInResult + this.textToReturnLowerCase(lowerCaseStrings)
+    if (upperCaseStrings.length > 0) textToSetInResult = textToSetInResult + this.textToReturnUpperCase(upperCaseStrings)
+
+    this.result = textToSetInResult
+  }
 }
+
